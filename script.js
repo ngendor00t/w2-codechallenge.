ab8c3f3c9a7d9979d48876b02576834cb2127380
Reginawambui <ngendoanita8@gmail.com>
2023-10-15 21:02:33 +0300

    completed assingments

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 async function logPets() {
 const response = await fetch("https://my-json-server.typicode.com/ngendor00t/pets-server/db");
+//i used my own link after creating it i fetched the data for the animals.
+//i used the modern way for fetching after creating the function pets
     const petData = await response.json();
-    console.log(petData.characters); 
+    console.log(petData.characters);
+//after waiting for a responsce via j.son i console logged petdata to see if it exists
+
     const { characters }   = petData;        
     for (let pet of characters) {
         toAppend.innerHTML += `
@@ -13,10 +17,20 @@ const response = await fetch("https://my-json-server.typicode.com/ngendor00t/pet
     <h2>${pet.votes}</h2>
     
     
+    
     </div>
-    `;    
+    `; 
+    // display the fetched to html 
+    // i researched this since the for loop and fetch didnt connect to my local host
+
 
   }
 }
 
 logPets()
+//i console logged the pets
+ 
+function onButtonClick(){
+    alert('button click');
+}
+document.querySelector('h1');
